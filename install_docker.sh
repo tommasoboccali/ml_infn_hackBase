e0e7419071dd9d041a45438f101c7fa6f75b3a7d
Tommsao Boccali <tommaso.boccali@cern.ch>
2021-06-01 10:00:59 +0200

    scripts to create areas

--- install_docker.sh
+++ install_docker.sh
@@ -8,8 +8,13 @@ myname = $MYNAME
 #apt-get install -u subversion
 
 cd ${vmshare}
-mkdir -p $myname
-cd $myname
-svn checkout svn export https://github.com/tommasoboccali/ml_infn_hackBase/trunk/VM_SHARED
+if [ -d "${vmshare}/${myname}" ] 
+then
+    echo "Directory /path/to/dir exists." 
+else
+ mkdir -p $myname
+ cd $myname
+ svn checkout svn export https://github.com/tommasoboccali/ml_infn_hackBase/trunk/VM_SHARED
+fi
 
 
