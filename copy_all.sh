b56919a5624be7c6bb3408b9e48297c79fd23c08
Tommsao Boccali <tommaso.boccali@cern.ch>
2021-06-03 12:17:53 +0200

    add autosave=0 for the hackathon

---
+++ copy_all.sh
@@ -0,0 +1,8 @@
+cd /Users/tom/github/ml_infn_hackBase
+cp virgo.ipynb VM_COLLABSHARED/GW/
+cp virgo.ipynb VM_BACKUP/VM_COLLABSHARED/GW/
+cp btaggingTom_May26_nocelloutput* VM_COLLABSHARED/HEP/
+cp btaggingTom_May26_nocelloutput* VM_BACKUP/VM_COLLABSHARED/HEP/
+cp DL_classification_mammocalcification_0106.ipynb DL_classification_mammocalcification_STUDENT.ipynb VM_COLLABSHARED/FISMED/
+cp DL_classification_mammocalcification_0106.ipynb DL_classification_mammocalcification_STUDENT.ipynb VM_BACKUP/VM_COLLABSHARED/FISMED/
+
