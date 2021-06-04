1d3455b3e448bb871478a012040fe1a9288efd61
Tommsao Boccali <tommaso.boccali@cern.ch>
2021-06-04 12:52:07 +0200

    fix all areas

--- copy_all.sh
+++ copy_all.sh
@@ -5,4 +5,10 @@ cp btaggingTom_May26_nocelloutput* VM_COLLABSHARED/HEP/
 cp btaggingTom_May26_nocelloutput* VM_BACKUP/VM_COLLABSHARED/HEP/
 cp DL_classification_mammocalcification_0106.ipynb DL_classification_mammocalcification_STUDENT.ipynb VM_COLLABSHARED/FISMED/
 cp DL_classification_mammocalcification_0106.ipynb DL_classification_mammocalcification_STUDENT.ipynb VM_BACKUP/VM_COLLABSHARED/FISMED/
-
+# handson
+cp 1DCNN/*.ipynb VM_BACKUP/VM_SHARED/BOMBINI/
+cp 1DCNN/*.ipynb VM_SHARED/BOMBINI/
+cp lucio*.ipynb VM_BACKUP/VM_SHARED/ANDERLINI
+cp lucio*.ipynb VM_SHARED/ANDERLINI
+cp Simple*.ipynb VM_SHARED/RIZZI
+cp Simple*.ipynb VM_BACKUP/VM_SHARED/RIZZI
