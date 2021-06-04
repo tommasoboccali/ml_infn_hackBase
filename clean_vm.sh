7b86e35720acbb2f7b2fe5de98e597018e7e53d0
Tommsao Boccali <tommaso.boccali@cern.ch>
2021-06-04 10:44:21 +0200

    clean to restore areas

---
+++ clean_vm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# fix Backup
+echo Restoring  Backup
+cd /jupyter-users/Backup
+rm -rf VM_COLLABSHARED  VM_SHARED
+svn update
+
+# fix collabshared
+echo Restoring  collabspace
+cd /usr/local/share/collabspace
+rm -rf FISMED  GW  HEP
+svn update
+
+# kill all the user areas
+echo Restoring  shared
+cd /jupyter-users/shared
+for i in $(ls -1)
+do
+    cd $i
+    echo === clean $i
+    rm -rf ANDERLINI  BOMBINI  RIZZI
+    svn update
+    cd ..
+done
