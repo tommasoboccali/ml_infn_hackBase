c0fcba5185e7ab063dbbb655779ff6afb6fc6a01
Tommsao Boccali <tommaso.boccali@cern.ch>
2021-12-06 13:26:21 +0100

    version for december

--- copy_all.sh
+++ copy_all.sh
@@ -1,8 +1,10 @@
 cd /Users/tom/github/ml_infn_hackBase
 cp virgo.ipynb VM_COLLABSHARED/GW/
 cp virgo.ipynb VM_BACKUP/VM_COLLABSHARED/GW/
-cp btaggingTom_May26_nocelloutput* VM_COLLABSHARED/HEP/
-cp btaggingTom_May26_nocelloutput* VM_BACKUP/VM_COLLABSHARED/HEP/
+cp btaggingTom_May26_nocelloutput* VM_COLLABSHARED/HEP_BTAG/
+cp btaggingTom_May26_nocelloutput* VM_BACKUP/VM_COLLABSHARED/HEP_BTAG/
+cp  HiggsSearchesBru_Nov14_nocelloutpu*ipynb VM_COLLABSHARED/HEP_BTAG/
+cp  HiggsSearchesBru_Nov14_nocelloutpu*ipynb VM_BACKUP/VM_COLLABSHARED/HEP_BTAG/
 cp DL_classification_M* VM_COLLABSHARED/FISMED/
 cp DL_classification_M* VM_BACKUP/VM_COLLABSHARED/FISMED/
 # handson
