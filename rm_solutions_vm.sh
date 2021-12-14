35cbf3cd9c3aab070314cae0996d67fb30b2ac65
Tommsao Boccali <tommaso.boccali@cern.ch>
2021-12-14 16:40:51 +0100

    version for december

---
+++ rm_solutions_vm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# create collabshared
+echo Creating  collabspace
+cd /jupyter-mounts/collaborativefolder/
+rm -f VM_COLLABSHARED/GW/virgo.ipynb
+rm -f VM_COLLABSHARED/HEP_HIGGS/HiggsSearchesBru_Nov14_nocelloutput.ipynb
+rm -f VM_COLLABSHARED/HEP_BTAG/btaggingTom_May26_nocelloutput.ipynb
+rm -f VM_COLLABSHARED/FISMED/DL_classification_MedPhys.ipynb
+
+
