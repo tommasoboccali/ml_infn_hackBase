aa9f52a5209d26afbb94ef4725a387b7e423f243
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2023-06-20 16:00:12 +0200

    update for hackathon 2023

--- create_vm.sh
+++ create_vm.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+apt-get update
+apt-get install -y subversion
+
 # fix Backup
 echo Creating  Backup
-mkdir -p /jupyter-mount/Backup
+mkdir -p /jupyter-mounts/Backup
 #rm -rf VM_COLLABSHARED  VM_SHARED
-cd  /jupyter-mount/Backup
+cd  /jupyter-mounts/Backup
 svn checkout https://github.com/tommasoboccali/ml_infn_hackBase/trunk/VM_BACKUP
 
 # create collabshared
 echo Creating  collabspace
 cd /jupyter-mounts/collaborativefolder/
-svn checkout https://github.com/tommasoboccali/ml_infn_hackBase/trunk/VM_COLLABSHARED
+svn checkout https://github.com/tommasoboccali/ml_infn_hackBase/trunk/VM_COLLABSHARED Exercises
 
 # create user areas
 echo Create user areas
@@ -19,6 +22,6 @@ for i in $(ls -1)
 do
     cd $i
     echo === generate $i
-    svn checkout https://github.com/tommasoboccali/ml_infn_hackBase/trunk/VM_SHARED
+    svn checkout https://github.com/tommasoboccali/ml_infn_hackBase/trunk/VM_SHARED Lectures
     cd ..
 done
